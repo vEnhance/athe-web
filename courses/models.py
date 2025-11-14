5962191d677427326dfc3865e2c87f0e6f4b7ba7
Evan Chen <evan@evanchen.cc>
2025-11-13 22:23:33 -0800

    feat: courses model

--- courses/models.py
+++ courses/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from home.models import StaffPhotoListing
 
 
 class Semester(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
+    start_date = models.DateTimeField(help_text="When this semester starts")
+    end_date = models.DateTimeField(help_text="When this semester ends")
 
     def __str__(self) -> str:
         return self.name
@@ -14,6 +17,21 @@ class Course(models.Model):
     description = models.TextField()
     semester = models.ForeignKey(
         Semester, on_delete=models.CASCADE, related_name="courses"
+    )
+    instructor = models.ForeignKey(
+        StaffPhotoListing,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="courses",
+        help_text="Link to the instructor for this course.",
+    )
+    difficulty = models.CharField(
+        blank=True,
+        max_length=80,
+        help_text="Estimate of the difficulty of this course.",
+    )
+    lesson_plan = models.TextField(
+        blank=True, help_text="List of lessons planned for this course. One per line."
     )
 
     def __str__(self) -> str:
