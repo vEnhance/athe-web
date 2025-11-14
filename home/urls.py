8cd82e23581d9c3ab7008a5c702a3f16051ece7b
Evan Chen <evan@evanchen.cc>
2025-11-13 20:12:23 -0800

    chore: ruff format

--- home/urls.py
+++ home/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
     path("staff/edit/", views.StaffPhotoUpdateView.as_view(), name="staff_edit"),
     path("donors/", views.DonorsView.as_view(), name="donors"),
     path("history/", views.HistoryView.as_view(), name="history"),
-    path("virtual-program/", views.VirtualProgramView.as_view(), name="virtual_program"),
+    path(
+        "virtual-program/", views.VirtualProgramView.as_view(), name="virtual_program"
+    ),
     path("scholarships/", views.ScholarshipsView.as_view(), name="scholarships"),
     path("past-psets/", views.PastPsetsView.as_view(), name="past_psets"),
     path("legal/", views.LegalView.as_view(), name="legal"),
