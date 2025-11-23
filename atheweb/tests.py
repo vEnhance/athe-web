5fc990719c930158e2cf7bd465ebf6c3e4cea7b7
Evan Chen <evan@evanchen.cc>
2025-11-22 20:28:53 -0500

    fix: login grief

--- atheweb/tests.py
+++ atheweb/tests.py
@@ -6,5 +6,5 @@ from django.test import Client
 @pytest.mark.django_db
 def test_login_works():
     client = Client()
-    response = client.get(reverse("account_login"))
+    response = client.get(reverse("login"))
     assert response.status_code == 200
