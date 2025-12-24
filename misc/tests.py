e9951fd3e380b9b0da48b7e006e72f99f1439033
Evan Chen <evan@evanchen.cc>
2025-12-24 11:59:47 -0800

    tests(misc): make sure the view works

--- misc/tests.py
+++ misc/tests.py
@@ -1,1 +1,8 @@
-# Create your tests here.
+from django.urls import reverse
+from django.test import Client
+
+
+def test_login_works():
+    client = Client()
+    response = client.get(reverse("misc:sp26game"))
+    assert response.status_code == 200
