db6c4a5c9c0a5992c6b583cabd0c3ab4dc7c6a7d
Evan Chen <evan@evanchen.cc>
2026-03-14 10:47:14 -0400

    feat: logging in gunicorn

--- gunicorn.sh
+++ gunicorn.sh
@@ -3,7 +3,16 @@
 set -euo pipefail
 umask 002
 
+log() {
+  local msg="$1"
+  local ts commit
+  ts=$(date +"%F %T %Z")
+  commit=$(git rev-parse --short HEAD 2>/dev/null || echo "unknown")
+  echo "[$ts] [$commit] $msg"
+}
+
 reload() {
+  log "♻️ RELOAD TRIGGERED. Every day I'm shuffling 🔀"
   uv run --no-sync python3 manage.py migrate
   kill -HUP "$GUNICORN_PID"
 }
@@ -12,6 +21,9 @@ trap reload HUP
 
 # We do NOT run uv sync as web user (will cause perm issues)
 # That should be handled by the git post-receive hook
+
+log "☀️  GOOD MORNING, my glorious webmaster! How are you this fine $(date +"%A in %B")?"
+log "🩷 Please enjoy these logs, I wrote them just for you 💌🖋️"
 
 # Initial migrate
 uv run --no-sync python3 manage.py migrate
