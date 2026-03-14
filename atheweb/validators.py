964d59b6558623bf207b6bc6d323e82df0406ce9
Evan Chen <evan@evanchen.cc>
2026-03-14 01:08:16 -0400

    fix: update markdown validators
    after bump to django-markdownfield

--- atheweb/validators.py
+++ atheweb/validators.py
@@ -1,6 +1,8 @@
 from markdownfield.validators import Validator, MARKDOWN_TAGS, MARKDOWN_ATTRS
 
+assert isinstance(MARKDOWN_TAGS, set)
+
 VALIDATOR_WITH_FIGURES = Validator(
-    allowed_tags=MARKDOWN_TAGS + ["figure", "figcaption"],
-    allowed_attrs={**MARKDOWN_ATTRS, "*": ["id", "class"]},
+    allowed_tags=MARKDOWN_TAGS.union({"figure", "figcaption"}),
+    allowed_attrs={**MARKDOWN_ATTRS, "*": {"id", "class"}},
 )
