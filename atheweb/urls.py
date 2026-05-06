340cf87ee4154734b341d7b709907953627a48f8
Evan Chen <evan@evanchen.cc>
2026-05-05 22:20:54 -0400

    feat: change admin site title

--- atheweb/urls.py
+++ atheweb/urls.py
@@ -57,3 +57,6 @@ urlpatterns = [
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.index_title = "Uncs Unauthorized"
+admin.site.site_title = "Athemath Administration"
